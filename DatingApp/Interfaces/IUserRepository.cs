01dc0857270e2498f9879eb72f453d3c5ebe8d97
Eugene Nesterov <en170901@gmail.com>
2023-01-19 01:44:48 +0300

    Add simple user interface

--- DatingApp/Interfaces/IUserRepository.cs
+++ DatingApp/Interfaces/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace DatingApp.Interfaces
     public interface IUserRepository
     {
         void Update(AppUser user);
-        Task<bool> SaveAllAsync();
+        Task<int> SaveAllAsync();
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
